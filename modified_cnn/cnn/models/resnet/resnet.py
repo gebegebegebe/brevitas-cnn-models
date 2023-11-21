44c372c094452696a0afe23f73b53faef5a6aff3
Gabriel Kaunang <72191858+gebegebegebe@users.noreply.github.com>
2023-11-21 21:52:22 +0700

    another update

--- modified_cnn/cnn/models/resnet/resnet.py
+++ modified_cnn/cnn/models/resnet/resnet.py
@@ -46,7 +46,7 @@ class QuantBottleneckBlock(nn.Module):
         self.conv3 = make_quant_conv2d(planes,self.expansion * planes,kernel_size=1,stride=1,padding=0,bias=bias,weight_bit_width=weight_bit_width)
         self.bn3 = nn.BatchNorm2d(planes * self.expansion)
         self.downsample = nn.Sequential()
-        self.relu3 = qnn.QuantReLU(bit_width=act_bit_width, return_quant_tensor=True)
+        #self.relu3 = qnn.QuantReLU(bit_width=act_bit_width, return_quant_tensor=True)
         #self.dropout = nn.Dropout(0.3)
         if stride != 1 or in_planes != self.expansion * planes:
             self.downsample = nn.Sequential(
@@ -66,7 +66,7 @@ class QuantBottleneckBlock(nn.Module):
             shared_quant_act = self.downsample[-1]
         if shared_quant_act is None:
             shared_quant_act = qnn.QuantReLU(bit_width=act_bit_width, return_quant_tensor=True)
-        self.relu3 = shared_quant_act
+        #self.relu3 = shared_quant_act
         self.relu_out = qnn.QuantReLU(return_quant_tensor=True, bit_width=act_bit_width)
         #self.adder = QuantIdentity(bit_width=act_bit_width, return_quant_tensor=True)
         self.adder = QuantIdentity(return_quant_tensor=False)
